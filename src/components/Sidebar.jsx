b677c88214a1e9751a19da0b07ec1eae0c8bb1e8
Suresh Alabani <94789102+SURESH2218@users.noreply.github.com>
2024-08-20 12:25:38 +0530

    fixed the bug of sidebar not opening

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,30 +4,42 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
   return isMenuOpen ? (
-    <div className="p-5 shadow-lg border flex flex-col gap-3 min-w-52 h-[89.5vh] ">
-      <ul className="">
-        <Link to="/">
-          <li>Home</li>
-        </Link>
-        <li>Shorts</li>
-        <li>Videos</li>
-        <li>Live</li>
-      </ul>
-      <ul>
-        <h1 className="font-bold">Subscriptions</h1>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Moving</li>
-      </ul>
-      <ul>
-        <h1 className="font-bold">Watch Later</h1>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Moving</li>
-      </ul>
+    <div className="p-5 shadow-lg border flex flex-col gap-3 min-w-52 h-[89.5vh]">
+      {/* Navigation Section */}
+      <section>
+        <ul className="">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>Shorts</li>
+          <li>Videos</li>
+          <li>Live</li>
+        </ul>
+      </section>
+
+      {/* Subscriptions Section */}
+      <section>
+        <h2 className="font-bold">Subscriptions</h2>
+        <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+      </section>
+
+      {/* Watch Later Section */}
+      <section>
+        <h2 className="font-bold">Watch Later</h2>
+        <ul>
+          <li>Music</li>
+          <li>Sports</li>
+          <li>Gaming</li>
+          <li>Movies</li>
+        </ul>
+      </section>
     </div>
   ) : null;
 };
--- src/components/VideoContainer.jsx
+++ src/components/VideoContainer.jsx
@@ -2,12 +2,16 @@ import React, { useEffect, useState } from "react";
 import { YOUTUBE_API_KEY } from "../utils/constants";
 import VideoCard, { NewVideoCard } from "./VideoCard";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { openMenu } from "../utils/appSlice";
 
 const VideoContainer = () => {
   const [video, setVideos] = useState([]);
+  const dispatch = useDispatch();
   useEffect(() => {
     getVideos();
-  }, []);
+    dispatch(openMenu());
+  }, [dispatch]);
   const getVideos = async () => {
     const data = await fetch(YOUTUBE_API_KEY);
     const json = await data.json();
@@ -16,7 +20,7 @@ const VideoContainer = () => {
   };
   return (
     <div className="flex flex-wrap">
-      {/* {video[3] && <NewVideoCard info={video[5]} />} */}
+      {/* {video[3] && <NewVideoCard info={video[9]} />} */}
       {video.map((videos, id) => (
         <Link key={id} to={"/watch?y=" + videos.id}>
           <VideoCard key={id} info={videos} />
